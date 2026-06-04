2469575f1c4ae4641e39b650832f3df9c766970b
Dervis van Leersum <dervis.vanleersum@soprasteria.com>
2026-06-04 22:18:47 +0200

    added constants

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Project config file for constants
+
+export const MAIN_NAME = "Dervis van Leersum";
+export const FORM_ENDPOINT = "https://submit-form.com/8HvIE4X9K";
+export const EMAIL = "dervis.vanleersum@gmail.com";
